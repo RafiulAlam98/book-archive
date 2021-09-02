863246c0fb16a8ef4295fef983516439cb574fd6
RafiulAlam <mrafiul.alam7@gmail.com>
2021-09-02 06:32:35 +0600

    style-added

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
 /* load data from API  */
-
 const loadData = data => {
+
    // input field created
   const inputField = document.getElementById('input-field')
   const inputValue = inputField.value
+
   // clear input 
   inputField.value = ''
+
   displaySpinner('block')
-  // displaySearchResult('none')
+  displayErrorMessage('none')
+
   const url =`http://openlibrary.org/search.json?q=${inputValue}`;
   fetch(url)
   .then(res => res.json())
   .then(data => loadUser(data.docs))
 }
 
-/* spinner handling */
 
-const displaySpinner = (displayStyle) =>{
+/* spinner handling */
+const displaySpinner = displayStyle =>{
   document.getElementById("spinner").style.display = displayStyle;
 }
-const displayErrorMessage = (displayStyle) =>{
+
+/* Error message handling */
+const displayErrorMessage = displayStyle =>{
   document.getElementById("error-handle").style.display = displayStyle;
 }
-// const displaySearchResult = (displayStyle) =>{
-//   document.getElementById("div-container").style.display = displayStyle;
-// }
 
-
-
+/* User Loaded */
 const loadUser = books =>{
   const divContainer = document.getElementById('div-container')
   divContainer.textContent = ''
   if(books.length === 0){
-    console.log('no item found')
     displayErrorMessage('block')
   }
   books?.forEach(book => {
@@ -40,7 +40,7 @@ const loadUser = books =>{
     div.classList.add('col')
     div.innerHTML = `
         <div class="card">
-          <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+          <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-fluid rounded "  alt="...">
           <div class="card-body">
             <h5 class="card-title">Name: ${book.title}</h5></h5>
             <h5 class="card-title">
@@ -59,5 +59,4 @@ const loadUser = books =>{
     
   });
   displaySpinner('none')
-  // displaySearchResult('block')
 }
